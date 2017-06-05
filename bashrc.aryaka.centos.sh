0ba38d903e7c6903ed4de69f0b0e6fcabd794bb5
lakshman kumar <lakshmankumar@gmail.com>
2017-06-05 15:57:50 -0700

    source centos-aryaka

---
+++ bashrc.aryaka.centos.sh
@@ -0,0 +1,5 @@
+alias python27="$HOME/install/python27/bin/python2.7"
+alias python2.7="$HOME/install/python27/bin/python2.7"
+export PYTHONPATH="$HOME/local/lib/python2.6/site-packages:$HOME/install/python27/lib/python2.7/site-packages"
+export PERL5LIB="$HOME/install/perl/lib"
+
