a01463cec64ce857f57340f86ff11258feba6483
lakshman kumar <lakshmankumar@gmail.com>
2017-06-07 11:14:40 -0700

    Updates from forge - keep it SVNGITROOT

--- bashrc.aryaka.centos.sh
+++ bashrc.aryaka.centos.sh
@@ -3,3 +3,16 @@ alias python2.7="$HOME/install/python27/bin/python2.7"
 export PYTHONPATH="$HOME/local/lib/python2.6/site-packages:$HOME/install/python27/lib/python2.7/site-packages"
 export PERL5LIB="$HOME/install/perl/lib"
 
+alias gwsa='cd $HOME/ws/git-dir-for-svn/git_asn'
+alias gwss='cd $HOME/ws/git-dir-for-svn/git_static/il3'
+alias gwsi='cd $HOME/ws/git-dir-for-svn/git_import'
+
+alias mil3='cd $HOME/ws/il3-scripts/il3_work_repo/il3/'
+alias mil3b='cd $HOME/ws/il3-scripts/il3_work_repo/il3/bin'
+alias mil3x='cd $HOME/ws/il3-scripts/il3_work_repo/il3/xml'
+
+alias il3='cd /usr/local/il3/'
+alias il3x='cd /usr/local/il3/xml'
+alias il3b='cd /usr/local/il3/bin'
+
+
