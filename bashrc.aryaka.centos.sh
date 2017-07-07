9853192bb932a6759c98563e5233b57c6210ce66
lakshman kumar <lakshmankumar@gmail.com>
2017-07-06 20:14:55 -0700

    move stuff into aryaka.bashrc

--- bashrc.aryaka.centos.sh
+++ bashrc.aryaka.centos.sh
@@ -1,5 +1,6 @@
 alias python27="$HOME/install/python27/bin/python2.7"
 alias python2.7="$HOME/install/python27/bin/python2.7"
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$HOME/install/clang/local/lib:$HOME/install/clang/local/lib.hide:$HOME/install/mosh/mosh_build/lib"
 export PERL5LIB="$HOME/install/perl/lib"
 
 alias gwsa='cd $HOME/ws/git-dir-for-svn/git_asn'
@@ -13,6 +14,13 @@ alias mil3x='cd $HOME/ws/il3-scripts/il3_work_repo/il3/xml.tim'
 alias il3='cd /usr/local/il3/'
 alias il3x='cd /usr/local/il3/xml'
 alias il3b='cd /usr/local/il3/bin'
+
+mkall()
+{
+  go
+  nohup make J=8 ANAPS=ace2 SYMBOLS_RPM=false anap-release pop pop-release RELEASE=optimized 2>&1 | tee make_op
+}
+export PATH="$HOME/install/rtags/rtags-2.10-install/wrap-bin:$PATH"
 
 alias gcomm='git commit -am "$(/home/lakshman_narayanan/gitlab/aryaka-new-clone/get_svn_commit_info.py)"'
 function nightly() {
@@ -37,4 +45,71 @@ rpmname() {
 
 rpmver() {
   rpm -q -i -p $(\ls -rt | tail -n 1 ) | grep Version | awk ' { print $3 } '
+}
+
+setw() {
+  shiftclone.py
+  if [ "$?" -eq "0" -a -f "$HOME/.shift_clone_result" ] ; then
+    export SVNGITROOT="$(cat $HOME/.shift_clone_result)"
+    cd "$SVNGITROOT"
+  fi
+}
+
+
+go () {
+  if [ -z "$SVNGITROOT" ] ; then
+    echo "SVNGITROOT not set.. Trying to guess by git root"
+    gr
+    echo "Its now: $SVNGITROOT"
+  else
+    if [[ $(pwd) != *"$SVNGITROOT"* ]] ; then
+      echo "pwd:$(pwd) doesn't contain SVNGITROOT: $SVNGITROOT"
+      return
+    fi
+  fi
+  if [ -n "$1" ] ; then
+     choice="$1"
+  else
+     choice="."
+  fi
+  cd $SVNGITROOT
+  if [ -f .branch_name ] ; then
+    cd $(cat .branch_name)
+  elif [ -n "$SVNBRANCH" ] ; then
+    cd "$SVNBRANCH"
+  else
+    echo "Neigher .branch_name nor SVNBRANCH is set"
+    return
+  fi
+  case $choice in
+    ar)
+      cd build.el6/ace2/anap_root
+      ;;
+    ai)
+      cd build.el6/anap/install
+      ;;
+    pi)
+      cd build.el6/pop/install
+      ;;
+    b)
+      cd build.el6
+      ;;
+    gr)
+      cd ../
+      ;;
+    am)
+      cd acehw/src/acemon
+      ;;
+    ah)
+      cd acehw
+      ;;
+    pns)
+      cd pns_ni
+      ;;
+    cs)
+      cd control/schema
+      ;;
+    *)
+      cd $choice
+  esac
 }
