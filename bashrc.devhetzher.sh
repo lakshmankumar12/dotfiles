8f3b913f2e5c8aaac4a8d3020eb5195c861f0689
lakshman kumar <lakshmankumar@gmail.com>
2023-03-07 16:58:41 +0530

    more configs

--- bashrc.devhetzher.sh
+++ bashrc.devhetzher.sh
@@ -249,6 +249,42 @@ scpdebtoanyagw() {
 scpdebtodagw() {
     scpdebtoanyagw "dagw"
 }
+scpdebtodagwstatic() {
+    scpdebtoanyagw "dagwstatic"
+}
+
+start_remote_access() {
+    if [ -z "$session_name" ] ; then
+        echo "set session_name"
+        return 1
+    fi
+    public_ip_of_machine=142.132.204.245
+    user=remoteagwuser
+    duration=7200
+    port=48555
+    notify_file=/home/remoteagwuser/${session_name}
+    MAGMA_ROOT=/magma_vm/genxcomm/onyx-corenw
+    ${MAGMA_ROOT}/lte/gateway/gxc_files/remote_access/prepare_instructions.sh -s $public_ip_of_machine -p 22 -u ${user} -l ${port} -n ${notify_file} -S ${session_name} -d ${duration} > /dev/null
+    cat key_${session_name}.pub  | tr -d '\n' | totmuxclip
+    echo "tmux clip is set with pub-key. Add this to remagwother"
+    echo "Later do"
+    echo 'cat instruction_to_copy | hhc'
+    echo 'ssh lakshman_remote_session'
+}
+
+
+#### BEGIN ORC FNS BLOCK
+
+set_local_orc8r() {
+    server=192.168.122.14
+    mykey=${MAGMA_ROOT}/orc8r/cloud/helm/orc8r/charts/secrets/certs/admin_operator.key.pem
+    mycert=${MAGMA_ROOT}/orc8r/cloud/helm/orc8r/charts/secrets/certs/admin_operator.pem
+    orcurl="anybody.magma.test:32630"
+    cacert_arg="--cacert"
+    rootcert=${MAGMA_ROOT}/orc8r/cloud/helm/orc8r/charts/secrets/certs/rootCA.pem
+    resolve_cmd="--resolve"
+    resolve_str="anybody.magma.test:32630:$server"
+}
 
 update_aws_kubecfg() {
     aws eks update-kubeconfig --region ${REGION} --name orc8r
@@ -274,10 +310,10 @@ set_qa_orc8r() {
     mykey=${dir}/admin_operator.key.pem
     mycert=${dir}/admin_operator.pem
     orcurl="api.generationxcomm.com"
-    resolve_cmd=""
-    resolve_str=""
     cacert_arg=""
     rootcert=""
+    resolve_cmd=""
+    resolve_str=""
 }
 
 set_svt_orc8r() {
@@ -292,3 +328,230 @@ set_svt_orc8r() {
     resolve_cmd=""
     resolve_str=""
 }
+
+set_prod_orc8r() {
+    REGION="us-east-2"
+    update_aws_kubecfg
+    dir=/magma_vm/aws_orc8rs/aws-magmaorc8r-prod/secrets_prod/certs
+    mykey=${dir}/admin_operator.key.pem
+    mycert=${dir}/admin_operator.pem
+    orcurl="api.genxcom.com"
+    cacert_arg=""
+    rootcert=""
+    resolve_cmd=""
+    resolve_str=""
+}
+
+post() {
+    echo "${newdata}" > /tmp/data.txt
+    cmd=$(echo curl --cert $mycert --key $mykey "${cacert_arg}" "$rootcert" -X POST "${resolve_cmd}" "${resolve_str}" "'$url'" -H "accept:application/json" -H "content-type:application/json" -d "@/tmp/data.txt")
+    echo "Running cmd: ${cmd}"
+    eval ${cmd}
+}
+put() {
+    echo "${newdata}" > /tmp/data.txt
+    cmd=$(echo curl --cert $mycert --key $mykey "${cacert_arg}" "$rootcert" -X PUT "${resolve_cmd}" "${resolve_str}" "'$url'" -H "accept:application/json" -H "content-type:application/json" -d "@/tmp/data.txt")
+    echo "Running cmd: ${cmd}"
+    eval ${cmd}
+}
+getn() {
+    rm -f /tmp/last_data
+    cmd=$(echo curl --cert $mycert --key $mykey "${cacert_arg}" "$rootcert" -X GET "${resolve_cmd}" "${resolve_str}" "'$url'" -H "accept:application/json")
+    echo "Running cmd: ${cmd}"
+    eval ${cmd} -o /tmp/last_data
+    data="$(cat /tmp/last_data | jq '.')"
+    newdata="${data}"
+}
+get() {
+    getn
+    echo "$data" | less
+}
+
+filedn() {
+    rm -f /tmp/last_data
+    cmd=$(echo curl --cert $mycert --key $mykey "${cacert_arg}" "$rootcert" -X GET "${resolve_cmd}" "${resolve_str}" "'$url'" -H "accept:application/octet-stream")
+    echo "Running cmd: ${cmd}"
+    eval ${cmd} -o /tmp/last_data
+    data="$(cat /tmp/last_data)"
+    echo "$data"
+}
+filednpg() {
+    filedn | less
+}
+del() {
+    cmd=$(echo curl --cert $mycert --key $mykey "${cacert_arg}" "$rootcert" -X DELETE "${resolve_cmd}" "${resolve_str}" "'$url'" -H "accept:application/json")
+    echo "Running cmd: ${cmd}"
+    eval ${cmd}
+}
+
+
+getwhat() {
+    what="${what#/}" ; if [ -n "$what" ] ; then what="/${what}" ; fi
+    url="${url}/${what}"
+    get
+}
+putwhat() {
+    what="${what#/}" ; if [ -n "$what" ] ; then what="/${what}" ; fi
+    url="${url}/${what}"
+    put
+}
+
+
+nw_all() {
+    url="https://${orcurl}/magma/v1/lte"
+}
+nw_one() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}"
+}
+nw_cert() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/ipsec/root_cert"
+}
+gw_all() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/gateways"
+}
+gw_one() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/gateways/${gwname}"
+}
+eb_all() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/enodebs"
+}
+eb_one() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/enodebs/${serial}"
+}
+eb_cert() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/enodebs/${serial}/ipsec/certificate"
+}
+eb_key() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/enodebs/${serial}/ipsec/priv_key"
+}
+eb_regen() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/enodebs/${serial}/ipsec/regenerate_certificate"
+}
+apn_all() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/apns"
+}
+apn_one() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/apns/${apnname}"
+}
+sub_all() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/subscribers"
+}
+sub_one() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/subscribers/${imsi}"
+}
+tenants_all() {
+    url="https://${orcurl}/magma/v1/tenants"
+}
+tier_all() {
+    url="https://${orcurl}/magma/v1/networks/${networkname}/tiers"
+}
+
+set_nw_to_vlan() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}"
+    getn
+    newdata=$(echo "$newdata" | jq '.cellular.epc.mobility.enable_multi_apn_ip_allocation = true | .cellular.epc.mobility.ip_allocation_mode = "DHCP_BROADCAST"')
+    put
+}
+set_nw_to_nat() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}"
+    getn
+    newdata=$(echo "$newdata" | jq '.cellular.epc.mobility.enable_multi_apn_ip_allocation = false | .cellular.epc.mobility.ip_allocation_mode = "NAT"')
+    put
+}
+set_vlan_gw_constants() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/gateways/${gwname}"
+    getn
+    newdata=$(echo "$newdata" | jq '.cellular.epc.nat_enabled = false | .cellular.epc.mesh_mgr_allocation_subnet = "192.168.202.0/24"' )
+    newdata=$(echo "$newdata" | jq '.apn_resources.MeshBackhaul = {"apn_name" : "MeshBackhaul", "gateway_ip" : "192.168.202.1", "gateway_mac" : "0c:0c:0c:0c:0c:0c", id: "myagw-MeshBackhaul", "vlan_id" : 202 }')
+    newdata=$(echo "$newdata" | jq '.apn_resources.testapn = {"apn_name" : "testapn", "gateway_ip" : "192.168.200.2", "gateway_mac" : "52:54:00:7a:74:a3", id: "myagw-testapn", "vlan_id" : 200, "local_ip": "192.168.200.10/24" }')
+}
+set_nat_gw_constants() {
+    url="https://${orcurl}/magma/v1/lte/${networkname}/gateways/${gwname}"
+    getn
+    newdata=$(echo "$newdata" | jq 'del(.apn_resources)')
+    newdata=$(echo "$newdata" | jq '.cellular.epc.nat_enabled = true | del(.cellular.epc.mesh_mgr_allocation_subnet) | del(.cellular.epc.sgi_management_iface_vlan)')
+}
+set_mgmt_dhcp() {
+    newdata=$(echo "$newdata" | jq 'del(.cellular.epc.sgi_management_iface_mode) | del(.cellular.epc.sgi_management_iface_static_ip) | del(.cellular.epc.sgi_management_iface_gw) | del(.cellular.epc.sgi_management_iface_dns_primary) | del(.cellular.epc.sgi_management_iface_dns_secondary) | del(.cellular.epc.sgi_management_iface_vlan)')
+}
+set_mgmt_native_vlan() {
+    newdata=$(echo "$newdata" | jq '.cellular.epc.sgi_management_iface_mode = "static" | .cellular.epc.sgi_management_iface_static_ip = "192.168.122.90/24" | .cellular.epc.sgi_management_iface_gw = "192.168.122.1" | .cellular.epc.sgi_management_iface_dns_primary = "8.8.4.4" | .cellular.epc.sgi_management_iface_dns_secondary = "8.8.8.8" | del(.cellular.epc.sgi_management_iface_vlan)')
+}
+set_mgmt_vlan_203() {
+    newdata=$(echo "$newdata" | jq '.cellular.epc.sgi_management_iface_mode = "static" | .cellular.epc.sgi_management_iface_static_ip = "192.168.203.5/24" | .cellular.epc.sgi_management_iface_gw = "192.168.203.1" | .cellular.epc.sgi_management_iface_dns_primary = "8.8.4.4" | .cellular.epc.sgi_management_iface_dns_secondary = "8.8.8.8" | .cellular.epc.sgi_management_iface_vlan = "203"')
+}
+set_testapn2_vlan_203_normal() {
+    newdata=$(echo "$newdata" | jq '.apn_resources.testapn2 = {"apn_name" : "testapn2", "gateway_ip" : "192.168.203.2", "gateway_mac" : "52:54:00:7a:74:a3", id: "myagw-testapn2", "vlan_id" : 203, "local_ip": "192.168.203.10/24" }')
+}
+set_testapn2_vlan_203_along_with_mgmt() {
+    newdata=$(echo "$newdata" | jq '.apn_resources.testapn2 = {"apn_name" : "testapn2", "gateway_ip" : "192.168.203.1", "gateway_mac" : "52:54:00:7a:74:a3", id: "myagw-testapn2", "vlan_id" : 203 }')
+}
+set_testapn2_vlan_0() {
+    newdata=$(echo "$newdata" | jq '.apn_resources.testapn2 = {"apn_name" : "testapn2", "gateway_ip" : "192.168.122.1", "gateway_mac" : "52:54:00:60:49:9c", id: "myagw-testapn2", "vlan_id" : 0 }')
+}
+
+set_vlan_agw_mgmt_0_apn_diff() {
+    set_nw_to_vlan
+    set_vlan_gw_constants
+    set_mgmt_native_vlan
+    set_testapn2_vlan_203_normal
+    put
+}
+set_vlan_agw_mgmt_0_apn_same_as_mgmt() {
+    set_nw_to_vlan
+    set_vlan_gw_constants
+    set_mgmt_native_vlan
+    set_testapn2_vlan_0
+    put
+}
+set_vlan_agw_mgmt_203() {
+    set_nw_to_vlan
+    set_vlan_gw_constants
+    set_mgmt_vlan_203
+    set_testapn2_vlan_203_along_with_mgmt
+    put
+}
+
+set_nat_agw_mgmt_static() {
+    set_nw_to_nat
+    set_nat_gw_constants
+    set_mgmt_native_vlan
+    put
+}
+set_nat_agw_mgmt_dhcp() {
+    set_nw_to_nat
+    set_nat_gw_constants
+    set_mgmt_dhcp
+    put
+}
+
+update_s1_ip() {
+    ip="$1"
+    newdata=$(echo "$newdata" | jq '.cellular.epc.s1_iface_static_ip = "'"$ip"'"' )
+}
+update_mmgr_anchor() {
+    ipnet="$1"
+    newdata=$(echo "$newdata" | jq '.cellular.epc.mesh_mgr_anchor_subnet = "'"$ipnet"'" ')
+}
+update_cobalt_assign() {
+    ipnet="$1"
+    newdata=$(echo "$newdata" | jq '.cellular.epc.cobalt_assignment_subnet = "'"$ipnet"'" ')
+}
+update_ueip() {
+    ipnet="$1"
+    newdata=$(echo "$newdata" | jq '.cellular.epc.ip_block = "'"$ipnet"'"')
+}
+update_mmgr_allocation() {
+    ipnet="$1"
+    newdata=$(echo "$newdata" | jq '.cellular.epc.mesh_mgr_allocation_subnet = "'"$ipnet"'"' )
+}
+update_ipsec_ap_net() {
+    ipnet="$1"
+    newdata=$(echo "$newdata" | jq '.cellular.ipsec.enodeb_ip_subnet = "'"$ipnet"'"')
+}
+update_ipsec_mmeip() {
+    ipnet="$1"
+    newdata=$(echo "$newdata" | jq '.cellular.ipsec.mme_local_ip = "'"$ipnet"'"')
+}
+
+#### END ORC FNS BLOCK
